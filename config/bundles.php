f0e2764d40c8d0213f705bc300ae285d0aec64fc
Grzegorz Matuszak <grzegorz.matuszak@tsh.io>
2020-01-28 08:10:42 +0100

    TweedeGolf Promethus integration

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,13 @@
 <?php
 
+use Snc\RedisBundle\SncRedisBundle;
+use TweedeGolf\PrometheusBundle\TweedeGolfPrometheusBundle;
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    TweedeGolfPrometheusBundle::class => ['all' => true],
+    SncRedisBundle::class => ['all' => true]
 ];
